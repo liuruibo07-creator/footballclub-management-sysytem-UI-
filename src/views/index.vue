ff00a8e3b82cb838f17e29baecb492b9226d6af1
lrb <2772156232@qq.com>
2026-08-27 20:22:23 +0800

    1

--- src/views/index.vue
+++ src/views/index.vue
@@ -4,7 +4,6 @@
       <div class="hero-shade" />
       <div class="hero-heading">
         <span id="next-match-title">下一场比赛</span>
-        <strong>中超联赛 第25轮</strong>
       </div>
 
       <div class="match-stage">
@@ -13,6 +12,7 @@
           <strong>天津津门虎</strong>
         </div>
         <div class="match-meta">
+          <span class="league-info">中超联赛 第25轮</span>
           <b>VS</b>
           <span><el-icon><Clock /></el-icon>2026-08-29&nbsp; 19:00</span>
           <span><el-icon><OfficeBuilding /></el-icon>天津泰达足球场</span>
@@ -222,6 +222,15 @@ function showMessage(target) {
   align-items: center;
   gap: 9px;
   color: rgba(255, 255, 255, 0.9);
+
+  .league-info {
+    font-size: 16px;
+    font-weight: 700;
+    color: #fff;
+    letter-spacing: 1px;
+    text-shadow: 0 2px 4px rgba(0, 0, 0, 0.3);
+    margin-bottom: 4px;
+  }
 
   b {
     margin-bottom: 3px;
